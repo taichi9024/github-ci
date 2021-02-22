9a8e516a4ed9097edc64a6baaf9b59e66d4dd552
tai <ebh48978@gmail.com>
2021-02-22 23:07:03 +0900

    最終調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root to: 'top#index'
   resources :rooms, only: %i[show index]
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    :registrations => 'users/registrations'
-  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/top_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class TopControllerTest < ActionDispatch::IntegrationTest
+  # test "should get index" do
+  #   get root_path
+  #   assert_response :success
+  # end
+end
